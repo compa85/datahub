dcec82353821f387ecb0364f0c35af0449f0d096
compa <dade.compagnoni@gmail.com>
2024-01-22 19:47:05 +0100

    Aggiunta file per test

---
+++ backend/test.php
@@ -0,0 +1,9 @@
+<?php
+include "database.php";
+
+$cinema = new Database("mariadb", "root", "root", "cinema");
+
+$json = file_get_contents("test.json");
+$object = json_decode($json);
+
+echo $cinema->delete($object);
