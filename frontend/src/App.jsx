c682b82efe2f05d53e8a653dd744e61388388bbe
compa85 <dade.compagnoni@gmail.com>
2024-01-26 18:44:53 +0100

    Installazione NextUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <div className="flex flex-col justify-center gap-4">
             <h1 className="text-4xl font-bold">Vite + React</h1>
             <button
-                className="rounded-lg px-3 py-2 transition hover:outline hover:outline-black dark:hover:outline-white"
+                className="rounded-lg px-3 py-2 transition hover:outline hover:outline-black"
                 onClick={() => setCount((count) => count + 1)}
             >
                 count is {count}
